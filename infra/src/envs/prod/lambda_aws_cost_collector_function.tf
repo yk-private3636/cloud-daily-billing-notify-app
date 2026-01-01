2004be5a092c5bd50f3b69320bcf323f02a80017
yk-private3636 <hsnysk318@gmail.com>
2026-01-01 17:31:14 +0900

    Lambda関数(aws_cost_collector)タイムアウト時間変更

--- infra/src/envs/prod/lambda_aws_cost_collector_function.tf
+++ infra/src/envs/prod/lambda_aws_cost_collector_function.tf
@@ -4,7 +4,7 @@ module "lambda_aws_cost_collector_func" {
   function_name = local.lambda_aws_cost_collector_function_name
   role_arn      = module.lambda_aws_cost_collector_role.arn
   image_uri     = "${module.ecr_repository.url}:${var.lambda_aws_cost_collector_image_tag}"
-  timeout       = 500
+  timeout       = 300
   memory_size   = 256
   architectures = ["x86_64"]
   environments = [
